9e59aaa4cbf097f82a6b9dbe9405da659f3da79d
Seb1Bastian <95043164+Seb1Bastian@users.noreply.github.com>
2025-01-05 12:19:19 +0100

    Add files via upload

--- Systemverilog/axi_wrapper.v
+++ Systemverilog/axi_wrapper.v
@@ -10,6 +10,8 @@ module axi_wrapper #(parameter KEY_WIDTH = 4,
     output ready_o,
     output valid_o,
     output [2+DATA_WIDTH+KEY_WIDTH-1:0] data_o );
+    
+wire [31:0] inbetween_data;
 
 
 hash_table #( .KEY_WIDTH(KEY_WIDTH),
@@ -25,9 +27,12 @@ the_table (
     .valid_i(valid_i),
     .ready_o(ready_o),
     .valid_o(valid_o),
-    .read_data_o(data_o[DATA_WIDTH-1:0]),
-    .no_deletion_target_o(data_o[28]),
-    .no_write_space_o(data_o[29]),
-    .no_element_found_o(data_o[30]),
-    .key_already_present_o(data_o[31]));
+    .read_data_o(inbetween_data[DATA_WIDTH-1:0]),
+    .no_deletion_target_o(inbetween_data[28]),
+    .no_write_space_o(inbetween_data[29]),
+    .no_element_found_o(inbetween_data[30]),
+    .key_already_present_o(inbetween_data[31]));
+    
+assign inbetween_data[27:26] = 2'b00;
+assign data_o = inbetween_data;
 endmodule
