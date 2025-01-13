9aebc0c20dad2c1e6c99b088bbe2a4f5729c53fc
Sebastian <sebastian.hilker@web.de>
2025-01-13 17:32:26 +0100

    flag reg for BRAM use in standard

--- Systemverilog/standard/flag_register.sv
+++ Systemverilog/standard/flag_register.sv
@@ -1,6 +1,7 @@
 module flag_register #(
     parameter SIZE = 10,
-    parameter BUCKET_SIZE = 1
+    parameter BUCKET_SIZE = 1,
+    parameter USE_MORE_BCLK = 1
 )
 (
     input logic                     clk,
@@ -17,25 +18,73 @@ module flag_register #(
     output logic [BUCKET_SIZE-1:0] flag_out_2
 );
 
-logic [BUCKET_SIZE-1:0] valid_flags [(2**SIZE)-1:0];
+generate
+    if (USE_MORE_BCLK == 1) begin
+        simple_dual_one_clock 
+            #(.MEM_SIZE(SIZE),
+              .DATA_WIDTH(BUCKET_SIZE)
+        )
+        block_ram_0(
+            .clk(clk),
+            .ena(ready_i),     //probably needs not to be changed
+            .enb(ready_i),     //probably needs not to be changed
+            .wea((write_en[i])),
+            .addra(write_adr),
+            .addrb(read_adr_0),
+            .dia(write_is_valid),
+            .dob(flag_out_0)
+        );
 
-always @(posedge clk) begin
-    if (reset == 1) begin
-        valid_flags <= '{default: '0};
-    end else if (write_en) begin
-        valid_flags[write_adr] <= write_is_valid;
-    end
-end
-always @(posedge clk) begin
-    if (reset == 1) begin
-        flag_out_0 <= '{default: '0};
-        flag_out_1 <= '{default: '0};
-        flag_out_2 <= '{default: '0};
+        simple_dual_one_clock 
+            #(.MEM_SIZE(SIZE),
+              .DATA_WIDTH(BUCKET_SIZE)
+        )
+        block_ram_1(
+            .clk(clk),
+            .ena(ready_i),     //probably needs not to be changed
+            .enb(ready_i),     //probably needs not to be changed
+            .wea((write_en[i])),
+            .addra(write_adr),
+            .addrb(read_adr_1),
+            .dia(write_is_valid),
+            .dob(flag_out_1)
+        );
+
+        simple_dual_one_clock 
+            #(.MEM_SIZE(SIZE),
+              .DATA_WIDTH(BUCKET_SIZE)
+        )
+        block_ram_2(
+            .clk(clk),
+            .ena(ready_i),     //probably needs not to be changed
+            .enb(ready_i),     //probably needs not to be changed
+            .wea((write_en[i])),
+            .addra(write_adr),
+            .addrb(read_adr_2),
+            .dia(write_is_valid),
+            .dob(flag_out_2)
+        );
     end else begin
-        flag_out_0 <= valid_flags[read_adr_0];
-        flag_out_1 <= valid_flags[read_adr_1];
-        flag_out_2 <= valid_flags[read_adr_2];
+        logic [BUCKET_SIZE-1:0] valid_flags [(2**SIZE)-1:0];
+        always @(posedge clk) begin
+            if (reset == 1) begin
+                valid_flags <= '{default: '0};
+            end else if (write_en) begin
+                valid_flags[write_adr] <= write_is_valid;
+            end
+        end
+        always @(posedge clk) begin
+            if (reset == 1) begin
+                flag_out_0 <= '{default: '0};
+                flag_out_1 <= '{default: '0};
+                flag_out_2 <= '{default: '0};
+            end else begin
+                flag_out_0 <= valid_flags[read_adr_0];
+                flag_out_1 <= valid_flags[read_adr_1];
+                flag_out_2 <= valid_flags[read_adr_2];
+            end
+        end
     end
-end
+endgenerate;
    
 endmodule
