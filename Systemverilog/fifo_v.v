0e18ef32f14dd4368af169f3a6424ae5f39945a2
Sebastian <sebastian.hilker@web.de>
2025-03-25 18:17:59 +0100

    better fifo

--- Systemverilog/fifo_v.v
+++ Systemverilog/fifo_v.v
@@ -1,51 +1,43 @@
-module fifo #(parameter DATA_WIDTH = 8, parameter DEPTH = 16) (
-    input wire clk,
-    input wire reset,
-    input wire wr_en,
-    input wire rd_en,
-    input wire [DATA_WIDTH-1:0] data_in,
-    output reg [DATA_WIDTH-1:0] data_out,
-    output wire empty,
-    output wire full
+module fifo #(parameter DEPTH=8, DWIDTH=16)
+(
+        input               	rstn,               // Active low reset
+                            	clk,                // Clock
+                            	wr_en, 				// Write enable
+                            	rd_en, 				// Read enable
+        input      [DWIDTH-1:0] din, 				// Data written into FIFO
+        output reg [DWIDTH-1:0] dout, 				// Data read from FIFO
+        output              	empty, 				// FIFO is empty when high
+                            	full 				// FIFO is full when high
 );
 
-// Interne Signale
-reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
-reg [3:0] wr_ptr = 0; // Schreibzeiger
-reg [3:0] rd_ptr = 0; // Lesezeiger
-reg [4:0] count = 0;  // Zähler für die Anzahl der gespeicherten Daten
 
-// Daten schreiben
-always @(posedge clk) begin
-    if (reset) begin
-        wr_ptr = 0;
-        rd_ptr = 0;
-        count = 0;
+  reg [$clog2(DEPTH)-1:0]   wptr;
+  reg [$clog2(DEPTH)-1:0]   rptr;
+
+  reg [DWIDTH-1 : 0]    fifo[DEPTH-1:0];
+
+  always @ (posedge clk) begin
+    if (!rstn) begin
+      wptr <= 0;
     end else begin
-        if (wr_en && !rd_en && !full) begin
-            mem[wr_ptr] = data_in;
-            wr_ptr = wr_ptr + 1;
-            count = count + 1;
-         end else if (!wr_en && rd_en && !empty) begin
-            rd_ptr = rd_ptr + 1;
-            count = count - 1;
-         end else if (wr_en && rd_en && !empty) begin
-            mem[wr_ptr] = data_in;
-            wr_ptr = wr_ptr + 1;
-            rd_ptr = rd_ptr + 1;
-         end else if (wr_en && rd_en && empty) begin
-            mem[wr_ptr] = data_in;
-            wr_ptr = wr_ptr + 1;
-            count = count + 1;
-         end
+      if (wr_en & !full) begin
+        fifo[wptr] <= din;
+        wptr <= wptr + 1;
+      end
     end
-end
+  end
 
-always @ (rd_ptr or mem) begin
-   data_out = mem[rd_ptr];
-end
-assign empty = (count == 0);
-assign full = (count == DEPTH);
+  always @ (posedge clk) begin
+    if (!rstn) begin
+      rptr <= 0;
+    end else begin
+      if (rd_en & !empty) begin
+        dout <= fifo[rptr];
+        rptr <= rptr + 1;
+      end
+    end
+  end
 
+  assign full  = (wptr + 1) == rptr;
+  assign empty = wptr == rptr;
 endmodule
-
